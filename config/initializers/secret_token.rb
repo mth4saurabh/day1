ca165d573bee20de14c967b058dc0f93ea25bb20
Saurabh <saurabhbansod1992@gmail.com>
2013-11-27 19:19:36 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwoSevenEleven::Application.config.secret_key_base = '35f5ee9081c7009debb6fe4a8d37f717864b1c065125f75f0ccc3ab9cb419e1f741bb69f2cec1ec18013d4b0b2f76a9fea198cebef68d9e1f85e7b98924a30c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwoSevenEleven::Application.config.session_store :cookie_store, key: '_two_seven_eleven_session'
